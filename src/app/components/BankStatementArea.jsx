af6db5be2a50043b6b71f20cb73a186b04f893a2
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-17 13:27:03 -0300

    stylization of banck statement components

--- src/app/components/BankStatementArea.jsx
+++ src/app/components/BankStatementArea.jsx
@@ -1,22 +1,23 @@
 export const BanckStatementArea = () => {
   return(
-    <div className="border-blue-600 shadow-lg justi items-center p-2 font-medium rounded-md">
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Valor</th>
-          <th>Tipo de Transação</th>
-          <th>Nome do Operador</th>
+    <table className="shadow-lg justi p-2 rounded-md mt-8 border-2 table-fixed">
+      <thead className="border-blue-600 border-2 rounded-xl">
+        <tr className="">
+          <th className="text-left p-2 border-blue-600 border-2">Data</th>
+          <th className="text-left p-2 border-blue-600 border-2">Valor</th>
+          <th className="text-left p-2 border-blue-600 border-2">Tipo de Transação</th>
+          <th className="text-left p-2 border-blue-600 border-2">Nome do Operador</th>
         </tr>
       </thead>
+
       <tbody>
         <tr>
-          <td>data</td>
-          <td>123456</td>
-          <td>tipo</td>
-          <td>operador</td>
+          <td className="p-2 border-blue-600 border-2">data</td>
+          <td className="p-2 border-blue-600 border-2">123456</td>
+          <td className="p-2 border-blue-600 border-2">tipo</td>
+          <td className="p-2 border-blue-600 border-2">operador</td>
         </tr>
       </tbody>
-    </div>
+    </table>
   )
 }
