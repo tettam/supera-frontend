753e71d5fd519b378a193be0f82cb6c63303d5c6
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-17 08:34:06 -0300

    add prop interaction by properties between components

--- src/app/components/FilterArea.jsx
+++ src/app/components/FilterArea.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react"
-
-export const FilterArea = () => {
-  const [startDate , setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [operatorName, setOperatorName] = useState('');
+export const FilterArea = ({filterData , setFilterData}) => {
+  const { startDate , endDate, operatorName} = filterData;
 
   return(
-    <div className="flex h-20 w-full -mt-10 rounded-md bg-fuchsia-800 font-medium">
+    <div className="flex h-20 w-full -mt-14 rounded-md overflow-hidden text-sm font-medium text-gray-900 group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
       <div className="flex flex-1 items-center justify-center flex-col">
         <div>
           <h3 className="text-white">Data de Inicío</h3>
@@ -15,8 +11,8 @@ export const FilterArea = () => {
             type="date" 
             name="dataInicio" 
             id="dataInicio"
-            onChange={(e) => setStartDate(e.target.value)}
-            className="text-slate-700 rounded-md p-1"
+            onChange={(e) => setFilterData({...filterData, startDate: e.target.value})}
+            className="text-slate-700 rounded-md p-1 outline-none"
           />
         </div>
       </div>
@@ -29,8 +25,8 @@ export const FilterArea = () => {
             type="date" 
             name="dataFinal" 
             id="dataFinal"
-            onChange={(e) => setEndDate(e.target.value)}
-            className="text-slate-700 rounded-md p-1"
+            onChange={(e) => setFilterData({...filterData, endDate: e.target.value})}
+            className="text-slate-700 rounded-md p-1 outline-none"
           />
         </div>
       </div>
@@ -43,8 +39,8 @@ export const FilterArea = () => {
             type="text"
             name="nomeOperadorTransicao" 
             id="nomeOperadorTransicao"
-            onChange={(e) => setOperatorName(e.target.value)}
-            className="text-slate-700 rounded-md p-1"
+            onChange={(e) => setFilterData({...filterData, startDate: e.target.value})}
+            className="text-slate-700 rounded-md p-1 outline-none w-36"
           />
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,29 @@
 'use client'
 
+import { useState } from "react";
+import { ButtonSearch } from "./components/ButtonSearch"
 import { FilterArea } from "./components/FilterArea"
 
 export default function Home() {
-  return (
-    <div>
-      <header className=" bg-black h-40 flex justify-center">
-        <h1 className="text-white font-bold text-3xl pt-4">Sistema Bancário</h1>
-      </header>
-      <div className="container flex max-w-screen-lg m-auto h-screen justify-center mb-20">
+  const [filters , setFilters] = useState({
+    startDate: null,
+    endDate: null,
+    operatorName: '',
+  })
 
-        <FilterArea />
-        
+  const handleClick = () => {
+    console.log(filters);
+  }
+
+  return (
+    <div className="">
+      <header className=" bg-black h-40 flex justify-center">
+        <h1 className="text-white font-bold text-4xl pt-4">Sistema Bancário</h1>
+      </header>
+      <div className="container flex max-w-screen-lg m-auto h-screen mb-20 p-4 flex-col">
+
+        <FilterArea filterData={filters} setFilterData={setFilters}/>
+        <ButtonSearch onClick={handleClick} />
       </div>
     </div>
   )
