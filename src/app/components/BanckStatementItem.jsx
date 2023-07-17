aed98c3fa9f3c864df5ccee80454add3c8a16272
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-17 13:33:39 -0300

    create component for each banck statement transaction

---
+++ src/app/components/BanckStatementItem.jsx
@@ -0,0 +1,10 @@
+export const BanckStatementItem = () => {
+  return (
+    <tr>
+      <td className="p-2 border-blue-600 border-2">data</td>
+      <td className="p-2 border-blue-600 border-2">123456</td>
+      <td className="p-2 border-blue-600 border-2">tipo</td>
+      <td className="p-2 border-blue-600 border-2">operador</td>
+    </tr>
+  )
+}
--- src/app/components/BankStatementArea.jsx
+++ src/app/components/BankStatementArea.jsx
@@ -1,22 +1,19 @@
+import { BanckStatementItem } from "./BanckStatementItem"
+
 export const BanckStatementArea = () => {
   return(
-    <table className="shadow-lg justi p-2 rounded-md mt-8 border-2 table-fixed">
-      <thead className="border-blue-600 border-2 rounded-xl">
+    <table className="shadow-lg justi p-2 mt-8 table-auto">
+      <thead className="border-blue-600 border-4 rounded-xl">
         <tr className="">
-          <th className="text-left p-2 border-blue-600 border-2">Data</th>
-          <th className="text-left p-2 border-blue-600 border-2">Valor</th>
-          <th className="text-left p-2 border-blue-600 border-2">Tipo de Transação</th>
-          <th className="text-left p-2 border-blue-600 border-2">Nome do Operador</th>
+          <th className="text-left p-2 border-blue-600 border-4">Data</th>
+          <th className="text-left p-2 border-blue-600 border-4">Valor</th>
+          <th className="text-left p-2 border-blue-600 border-4">Tipo de Transação</th>
+          <th className="text-left p-2 border-blue-600 border-4">Nome do Operador</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr>
-          <td className="p-2 border-blue-600 border-2">data</td>
-          <td className="p-2 border-blue-600 border-2">123456</td>
-          <td className="p-2 border-blue-600 border-2">tipo</td>
-          <td className="p-2 border-blue-600 border-2">operador</td>
-        </tr>
+        <BanckStatementItem />
       </tbody>
     </table>
   )
