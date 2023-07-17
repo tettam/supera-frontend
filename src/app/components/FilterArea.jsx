31722f6f45ed3ba784806dda16b13cd2b08e9d55
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-17 05:43:59 -0300

    Improved styling of filter components

--- src/app/components/FilterArea.jsx
+++ src/app/components/FilterArea.jsx
@@ -6,42 +6,45 @@ export const FilterArea = () => {
   const [operatorName, setOperatorName] = useState('');
 
   return(
-    <div className="flex h-16 w-full -mt-10 rounded-md bg-gray-300 font-medium">
+    <div className="flex h-20 w-full -mt-10 rounded-md bg-fuchsia-800 font-medium">
       <div className="flex flex-1 items-center justify-center flex-col">
         <div>
-          <h3>Data de Inicío</h3>
+          <h3 className="text-white">Data de Inicío</h3>
           <input 
             value={startDate}
             type="date" 
             name="dataInicio" 
             id="dataInicio"
             onChange={(e) => setStartDate(e.target.value)}
+            className="text-slate-700 rounded-md p-1"
           />
         </div>
       </div>
 
       <div className="flex flex-1 items-center justify-center flex-col">
         <div>
-          <h3 className="">Data Final</h3>
+          <h3 className="text-white">Data Final</h3>
           <input
             value={endDate}
             type="date" 
             name="dataFinal" 
             id="dataFinal"
             onChange={(e) => setEndDate(e.target.value)}
+            className="text-slate-700 rounded-md p-1"
           />
         </div>
       </div>
 
       <div className="flex flex-1 items-center justify-center flex-col">
         <div>
-          <h3 className="">Nome do operador</h3>
+          <h3 className="text-white">Nome do operador</h3>
           <input 
             value={operatorName}
             type="text"
             name="nomeOperadorTransicao" 
             id="nomeOperadorTransicao"
             onChange={(e) => setOperatorName(e.target.value)}
+            className="text-slate-700 rounded-md p-1"
           />
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { FilterArea } from "./components/FilterArea"
 export default function Home() {
   return (
     <div>
-      <header className=" bg-black h-32 flex justify-center">
+      <header className=" bg-black h-40 flex justify-center">
         <h1 className="text-white font-bold text-3xl pt-4">Sistema Bancário</h1>
       </header>
       <div className="container flex max-w-screen-lg m-auto h-screen justify-center mb-20">
